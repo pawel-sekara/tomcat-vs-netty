1c6e71b2dcbdeada059f0c1cae6fe0160c213be9
Paweł Sękara <pawel.sekara@cm.tech>
2024-05-21 00:26:46 +0200

    Initial

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+import de.fayard.refreshVersions.core.StabilityLevel
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
+rootProject.name = "blocking-vs-nonblocking"
+
+@Suppress("UnstableApiUsage")
+refreshVersions {
+    rejectVersionIf {
+        candidate.stabilityLevel != StabilityLevel.Stable
+    }
+}
+
